661cf12065e4ddd7186adad2228f569185893070
magisamericae <32869858+e000e@users.noreply.github.com>
2018-06-06 18:11:34 +0200

    1

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+$(".button").show();
+$(function() {
+
+  var inWrap = $('.inner-wrapper');
+
+  $('.prev').on('click', function() {
+
+    inWrap.animate({left: '0%'}, 800, function(){
+
+      inWrap.css('left', '-100%');
+
+      $('.slide').first().before($('.slide').last());
+
+    });
+
+
+  });
+
+
+
+  $('.next').on('click', function() {
+
+    inWrap.animate({left: '-200%'}, 800, function(){
+
+      inWrap.css('left', '-100%');
+
+      $('.slide').last().after($('.slide').first());
+
+    });
+
+
+  });
+
+
+})
